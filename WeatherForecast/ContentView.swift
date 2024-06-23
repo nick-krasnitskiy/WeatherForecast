b2e0b86eec572fb09ee52cf1d61649d55aee1df5
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-23 19:42:57 +0300

    Add a custom struct.

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", high: 70, low: 50)
+            DayForecast(day: "Tue", high: 60, low: 40)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let high: Int
+    let low: Int
+    
+    var body: some View {
+        VStack {
+            Text(day)
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("High: \(high)ºC")
+            Text("Low: \(low)ºC")
+        }
+        .padding()
+    }
 }
