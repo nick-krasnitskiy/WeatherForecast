ed4e37fa9a19cc1b2eae890ba6627c811400d98c
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-23 19:55:37 +0300

    Use computed properties for the icon and color.

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
-            DayForecast(day: "Tue", high: 60, low: 40)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
     }
 }
@@ -22,14 +22,31 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
     
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
             Text("High: \(high)ºC")
             Text("Low: \(low)ºC")
         }
