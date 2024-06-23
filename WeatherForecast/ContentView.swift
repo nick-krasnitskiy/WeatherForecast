ab6883668d1e1c9f1093040c1ae2fca70320f219
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-06-23 20:37:00 +0300

    Create a new custom WeekForecast view

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,15 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
-                DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
-                DayForecast(day: "San", isRainy: false, high: 90, low: 30)
-                DayForecast(day: "Thu", isRainy: true, high: 30, low: 10)
+        VStack(spacing: 30) {
+            ScrollView(.horizontal) {
+                HStack {
+                    DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+                    DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+                    DayForecast(day: "San", isRainy: false, high: 90, low: 30)
+                    DayForecast(day: "Thu", isRainy: true, high: 30, low: 10)
+                }
             }
+
+            WeekForecast(dateRange: "Mon - Thu:")
         }
-        
     }
 }
 
@@ -55,12 +58,51 @@ struct DayForecast: View {
                 .foregroundStyle(iconColor)
                 .font(.largeTitle)
                 .padding(5)
-            Text("High: \(high)ºC")
+            Text("High: \(high)ºF")
                 .fontWeight(.semibold)
-            Text("Low: \(low)ºC")
+            Text("Low: \(low)ºF")
                 .fontWeight(.medium)
                 .foregroundStyle(.secondary)
         }
         .padding()
+    }
+}
+
+struct WeekForecast: View {
+    let dateRange: String
+    
+    var averageLow: Int {
+        return (50 + 40 + 30 + 10) / 4
+    }
+
+    var averageHigh: Int {
+       return (70 + 60 + 90 + 30) / 4
+    }
+    
+    var numberOfSunDays: Int {
+        return 2
+    }
+    
+    var numberOfRainDays: Int {
+        return 2
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 5) {
+            Text(dateRange)
+                .font(.largeTitle)
+            Text("Average Low: \(averageLow)ºF")
+                .font(.title2)
+                .foregroundStyle(.secondary)
+            Text("Average High: \(averageHigh)ºF")
+                .font(.title2)
+                .foregroundStyle(.secondary)
+            Text("Number of Sun Days: \(numberOfSunDays)")
+                .font(.title2)
+                .foregroundStyle(.secondary)
+            Text("Number of Rain Days: \(numberOfRainDays)")
+                .font(.title2)
+                .foregroundStyle(.secondary)
+        }
     }
 }
